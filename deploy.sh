a3f1d34934dd7202249abce8491da4cbbc54ae8e
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2019-05-15 21:49:48 +0800

    fixed update script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 SCRIPT_PATH=$(dirname $(readlink -f $0))
-mkdir -p $SCRIPT_PATH/scripts
-cd $SCRIPT_PATH/scripts
 git init
 git config core.sparsecheckout true
 echo 'scripts' > .git/info/sparse-checkout
 git remote add -f origin git@git.loanstreet.com.my:loanstreet/deploy-scripts.git
 git pull origin master
 
-sh deploy.sh $1
+sh scripts/deploy.sh $1
