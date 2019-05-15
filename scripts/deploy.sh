c7d4733f221c265d230ca9f63e127d0cb77c4f0e
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2019-05-15 21:30:11 +0800

    updateable scripts moved to separate directory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 SCRIPT_PATH=$(dirname $(readlink -f $0))
-. $SCRIPT_PATH/app-config.sh
+. $SCRIPT_PATH/../app-config.sh
 
 if [ "$1" = "" ]; then
     echo "No environment set"
     exit
 else
     PROJECT_ENVIRONMENT="$1"
-    if [ ! -f $SCRIPT_PATH/$PROJECT_ENVIRONMENT/config.sh ]; then
+    if [ ! -f $SCRIPT_PATH/../$PROJECT_ENVIRONMENT/config.sh ]; then
         echo "Please initialize deploy/$PROJECT_ENVIRONMENT/config.sh with vars PROJECT_ENVIRONMENT, SERVICE_NAME and SERVICE_PORT"
         exit
     fi
-    . $SCRIPT_PATH/$PROJECT_ENVIRONMENT/config.sh
+    . $SCRIPT_PATH/../$PROJECT_ENVIRONMENT/config.sh
 fi
 
 TIMESTAMP=$(date +%s)
 BARE_REPO_SCRIPT_DIR=/tmp/deployer-$TIMESTAMP
 
 mkdir -p $BARE_REPO_SCRIPT_DIR
-cat $SCRIPT_PATH/app-config.sh $SCRIPT_PATH/$PROJECT_ENVIRONMENT/config.sh > $BARE_REPO_SCRIPT_DIR/config.sh
+cat $SCRIPT_PATH/../app-config.sh $SCRIPT_PATH/../$PROJECT_ENVIRONMENT/config.sh > $BARE_REPO_SCRIPT_DIR/config.sh
 cp $SCRIPT_PATH/bare-repo.sh $BARE_REPO_SCRIPT_DIR/
-cp $SCRIPT_PATH/$PROJECT_ENVIRONMENT/git-hook-post-receive $BARE_REPO_SCRIPT_DIR/
+cp $SCRIPT_PATH/../$PROJECT_ENVIRONMENT/git-hook-post-receive $BARE_REPO_SCRIPT_DIR/
 
 echo "Copying scripts to create bare git repo"
 scp -r $BARE_REPO_SCRIPT_DIR $DEPLOYMENT_SSH_USER@$DEPLOYMENT_SERVER:/tmp/
@@ -35,13 +35,13 @@ PROJECT_ENVIRONMENT=$PROJECT_ENVIRONMENT sh $SCRIPT_PATH/$BUILD.sh
 REMOTE_GIT_BARE_REPO=ssh://$DEPLOYMENT_SSH_USER@$DEPLOYMENT_SERVER/home/$DEPLOYMENT_SSH_USER/repos/$SERVICE_NAME/$PROJECT_ENVIRONMENT.git
 echo "Deploying $PROJECT_ENVIRONMENT to $REMOTE_GIT_BARE_REPO"
 
-if [ ! -d $SCRIPT_PATH/repo ]; then
+if [ ! -d $SCRIPT_PATH/../repo ]; then
 	echo "No deployment repo created by $BUILD script. exiting"
 	exit
 fi
-cd $SCRIPT_PATH/repo
+cd $SCRIPT_PATH/../repo
 git remote rm fincon-dev
 git remote add fincon-dev $REMOTE_GIT_BARE_REPO
 git push fincon-dev master -f
 echo "Deleting deployment repo $SCRIPT_PATH/repo"
-rm -rf $SCRIPT_PATH/repo
+rm -rf $SCRIPT_PATH/../repo
