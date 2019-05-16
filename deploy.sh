9cfac62ab8e4f2a4f45172703ee36aa053084439
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2019-05-16 17:44:12 +0800

    clean git checkout when updating deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 SCRIPT_PATH=$(dirname $(readlink -f $0))
+mkdir -p $SCRIPT_PATH/deploy-scripts
+cd $SCRIPT_PATH/deploy-scripts
 git init
 git config core.sparsecheckout true
 echo 'scripts' > .git/info/sparse-checkout
 git remote add -f origin git@git.loanstreet.com.my:loanstreet/deploy-scripts.git
 git pull origin master
+git --work-tree=../ --git-dir=.git checkout -f
+cd $SCRIPT_PATH
+rm -rf $SCRIPT_PATH/deploy-scripts
 
 sh scripts/deploy.sh $1
