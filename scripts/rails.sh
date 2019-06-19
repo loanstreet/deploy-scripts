a4186c69f8475929cc354bd548f9b11226583fb5
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2019-06-19 12:58:40 +0800

    no need to eval path when it can be read from string

--- scripts/rails.sh
+++ scripts/rails.sh
@@ -25,15 +25,15 @@ git --work-tree=../deploy-repo --git-dir=.git checkout -f 2>&1 | indent
 cd $PROJECT_DEPLOY_DIR/work/deploy-repo/config
 mkdir -p deploy
 cd deploy/
-PID_PATH='$DEPLOYMENT_DIR/$SERVICE_NAME/$PROJECT_ENVIRONMENT/current/tmp/pids/$SERVICE_NAME.pid'
-SOCKET_PATH='unix://$DEPLOYMENT_DIR/$SERVICE_NAME/$PROJECT_ENVIRONMENT/current/tmp/sockets/$SERVICE_NAME.sock'
+PID_PATH=$DEPLOYMENT_DIR'/$SERVICE_NAME/$PROJECT_ENVIRONMENT/current/tmp/pids/$SERVICE_NAME.pid'
+SOCKET_PATH='unix://'$DEPLOYMENT_DIR'/$SERVICE_NAME/$PROJECT_ENVIRONMENT/current/tmp/sockets/$SERVICE_NAME.sock'
 if [ "$SERVICE_PORT" = "" ]; then
 	COMMAND='$HOME/.rbenv/bin/rbenv exec bundle exec puma -C $DEPLOYMENT_DIR/$SERVICE_NAME/$PROJECT_ENVIRONMENT/current/config/puma.rb --environment=$PROJECT_ENVIRONMENT --bind=$SOCKET_PATH --pidfile=$PID_PATH --daemon && sleep 3'
 else
 	COMMAND='$HOME/.rbenv/bin/rbenv exec bundle exec puma -C $DEPLOYMENT_DIR/$SERVICE_NAME/$PROJECT_ENVIRONMENT/current/config/puma.rb --environment=$PROJECT_ENVIRONMENT -p $SERVICE_PORT --pidfile=$PID_PATH --daemon && sleep 3'
 fi
 cat $PROJECT_DEPLOY_DIR/app-config.sh $PROJECT_DEPLOY_DIR/$PROJECT_ENVIRONMENT/config.sh > config.sh
-echo 'eval DEPLOYMENT_DIR=$DEPLOYMENT_DIR' >> config.sh
+#echo 'eval DEPLOYMENT_DIR=$DEPLOYMENT_DIR' >> config.sh
 echo "PID_PATH=\"$PID_PATH\"" >> config.sh
 echo "SOCKET_PATH=\"$SOCKET_PATH\"" >> config.sh
 echo "START_COMMAND=\"$COMMAND\"" >> config.sh
