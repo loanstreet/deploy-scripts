08f506479e43d930216f2e1fccf8ae5ea130252d
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2019-08-22 14:47:23 +0800

    fixed install sccript and update logic

--- installer/install.sh
+++ installer/install.sh
@@ -49,7 +49,7 @@ if [ ! -d $DEPLOY_SCRIPTS_HOME ]; then
 	echo "Downloading deploy-scripts"
 	git clone --single-branch --depth=1 --branch $DEPLOY_SCRIPTS_GIT_BRANCH $DEPLOY_SCRIPTS_GIT_REPO $DEPLOY_SCRIPTS_HOME
 else
-	git fetch origin +refs/heads/$DEPLOY_SCRIPTS_GIT_BRANCH
+	cd $DEPLOY_SCRIPTS_HOME && git fetch origin +refs/heads/$DEPLOY_SCRIPTS_GIT_BRANCH && cd $SCRIPT_PATH
 fi
 PROJECT_DEPLOY_DIR=$SCRIPT_PATH sh $DEPLOY_SCRIPTS_HOME/deploy.sh $1
 EOF
