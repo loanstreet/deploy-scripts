fb54ede176e60dc95e49dc3e3d4beaeaf5440770
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2019-09-04 16:16:23 +0800

    updated maven-lib script to push artifacts

--- scripts/java-mvnw-lib.sh
+++ scripts/java-mvnw-lib.sh
@@ -22,18 +22,8 @@ WORK_DIR=$PROJECT_DEPLOY_DIR/work
 DEPLOY_REPO=$WORK_DIR/deploy-repo
 
 cd $WORK_DIR/repo
-./mvnw package -Dmaven.test.skip=true 2>&1 | indent
+./mvnw deploy -Dmaven.test.skip=true 2>&1 | indent
 title 'build - java - prepare-deployment'
-cd target/
-WARFILE=$(ls *RELEASE.war 2>/dev/null | head -n1)
-if [ "$WARFILE" = "" ]; then
-	WARFILE=$(ls *RELEASE.jar | head -n1)
-fi
-if [ "$WARFILE" = "" ]; then
-	error "No jar or war file found to deploy"
-fi
-
-cp $WARFILE $PROJECT_DEPLOY_DIR/work
 mkdir -p $DEPLOY_REPO
 cd $DEPLOY_REPO
 
@@ -48,7 +38,7 @@ git config user.email "techgroup@loanstreet.com.my"
 git fetch
 git checkout $GIT_PUSH_BRANCH
 git pull origin $GIT_PUSH_BRANCH
-cp $PROJECT_DEPLOY_DIR/work/$WARFILE ./
-git add $WARFILE 2>&1 | indent
+cp -r $WORK_DIR/repo/target/maven-repo/* ./
+git add . 2>&1 | indent
 git commit . -m "commit to repository" 2>&1 | indent
 git push origin $GIT_PUSH_BRANCH
