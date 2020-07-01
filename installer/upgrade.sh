437c87fa17a88e92acd32f660ee551ba8c217c35
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2020-07-01 17:35:02 +0800

    upgrade script should be able to migrate rails projects now

--- installer/upgrade.sh
+++ installer/upgrade.sh
@@ -11,8 +11,12 @@ fi
 
 . "$1/app-config.sh"
 
+PREFIX='deploy'
+
 if [ "$BUILD" = "" ]; then
 	error "Corrupt app-config.sh, no BUILD variable found"
+elif [ "$BUILD" = "rails" ]; then
+	PREFIX='config/deploy'
 fi
 
 TIMESTAMP=$(date +%s)
@@ -20,11 +24,11 @@ TMP_DEPLOY="/tmp/deploy-scripts-upgrade-$TIMESTAMP"
 
 mkdir -p $TMP_DEPLOY
 sh $SCRIPT_PATH/install.sh $BUILD $TMP_DEPLOY --no-post-install
-rm -rf $TMP_DEPLOY/deploy/environments/default
+rm -rf $TMP_DEPLOY/$PREFIX/environments/default
 
-cp "$1/app-config.sh" $TMP_DEPLOY/deploy/app-config.sh
+cp "$1/app-config.sh" $TMP_DEPLOY/$PREFIX/app-config.sh
 if [ -f "$1/requirements.txt" ]; then
-	cp "$1/requirements.txt" $TMP_DEPLOY/deploy/requirements.txt
+	cp "$1/requirements.txt" $TMP_DEPLOY/$PREFIX/requirements.txt
 fi
 
 info "Updating environments"
@@ -34,8 +38,8 @@ ENVIRONMENTS=$(ls -d */)
 
 for i in $ENVIRONMENTS; do
 	info "Copying $i"
-	cp -r "$i" $TMP_DEPLOY/deploy/environments/
-	cd "$TMP_DEPLOY/deploy/environments/$i"
+	cp -r "$i" $TMP_DEPLOY/$PREFIX/environments/
+	cd "$TMP_DEPLOY/$PREFIX/environments/$i"
 	mkdir assets
 	if [ -f "nginx.conf" ]; then
 		mv nginx.conf assets/
@@ -50,7 +54,7 @@ done
 info "Moving new deployment directory to old"
 cd "$1/.."
 mv deploy .deploy-old
-mv $TMP_DEPLOY/deploy ./
+mv $TMP_DEPLOY/$PREFIX ./
 success "done"
 
 success "Migrated old deployment structure in $1 to $(cat $SCRIPT_PATH/../.VERSION)"
