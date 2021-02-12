3c30dc5a8b27827bd4988587d0e79c4437b779b1
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2021-02-12 23:51:53 +0800

    make TLS-secured quick deployments safer and easier

--- tests/kubernetes.sh
+++ tests/kubernetes.sh
@@ -13,12 +13,12 @@ SERVICE_NAME="python-deploy-test"
 PROJECT_ENVIRONMENT="default"
 DEPLOYMENT_DIR="$TEST_WORKING_DIR/$SERVICE_NAME/$PROJECT_ENVIRONMENT"
 PROJECT_DEPLOY_DIR="$COPY_PROJECT_DIR/python-project/deploy"
-
+HOST="deploy-scripts.dev.finology.com.my"
 cp -r $SCRIPT_PATH/../projects/python/template/docker $PROJECT_DEPLOY_DIR/
 cp -r $SCRIPT_PATH/../projects/python/template/environments/$PROJECT_ENVIRONMENT/docker $PROJECT_DEPLOY_DIR/environments/$PROJECT_ENVIRONMENT/
 
 printf "\nKUBERNETES_NGINX_SERVICE_HOST=\"deploy-scripts.finology.com.my\"\nDS_DEBUG=true\nDEPLOYMENT_DIR=$TEST_WORKING_DIR\nDEPLOYMENT_SERVER=localhost\nDEPLOYMENT_SERVER_USER=$USER\nREPO=file://$COPY_PROJECT_DIR/python-project\nSERVICE_NAME=$SERVICE_NAME\nLINKED_FILES=\nLINKED_DIRS=\"\"\n" >> deploy/app-config.sh
-printf "PROJECT_ENVIRONMENT=$PROJECT_ENVIRONMENT\nGIT_BRANCH=master\nDOCKER_REGISTRY=https://dockerhub.finology.com.my\nPACKAGE=docker\nPUSH=docker\nPOST_PUSH=kubernetes\nKUBERNETES_CLUSTER=dev\nKUBERNETES_TLS=false\nKUBERNETES_INGRESS=ingress-dev\n" >> deploy/environments/default/config.sh
+printf "PROJECT_ENVIRONMENT=$PROJECT_ENVIRONMENT\nGIT_BRANCH=master\nDOCKER_REGISTRY=https://dockerhub.finology.com.my\nPACKAGE=docker\nPUSH=docker\nPOST_PUSH=kubernetes\nKUBERNETES_CLUSTER=dev\nKUBERNETES_TLS=true\nKUBERNETES_INGRESS=ingress-dev-letsencrypt-prod\nKUBERNETES_NGINX_SERVICE_HOST=$HOST\n" >> deploy/environments/default/config.sh
 cat deploy/app-config.sh
 cat deploy/environments/default/config.sh
 #sed -i 's/    image\: django_project\:latest//g' deploy/environments/default/docker/docker-compose.yml
@@ -27,9 +27,9 @@ rm -rf $TEST_WORKING_DIR
 
 PROJECT_DEPLOY_DIR=$PROJECT_DEPLOY_DIR sh $SCRIPT_PATH/../scripts/deploy.sh default
 cd $COPY_PROJECT_DIR
-sleep 20
+sleep 40
 title 'TEST - check web application'
-wget http://deploy-scripts.finology.com.my/
+wget "https://$HOST/"
 printf 'Checking index page contents ... '
 if [ $(grep -c 'The install worked successfully! Congratulations!' index.html) -eq 1 ]; then
 	success 'success!'
--- tests/resources/django_project/django_project/settings.py
+++ tests/resources/django_project/django_project/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "deploy-scripts.finology.com.my",
+    "deploy-scripts.dev.finology.com.my",
     "127.0.0.1",
     "localhost"
 ]
