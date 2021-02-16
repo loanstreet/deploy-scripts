cc6dc9836850ce4692c5893c183f1244b857b331
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2021-02-16 23:39:02 +0800

    verbosy copy for run.sh script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -162,7 +162,7 @@ INCLUDE_RUN_SH=$(echo $RESTART_COMMAND | grep 'run.sh' | wc -l)
 
 # If restart command used run.sh script, include it in the deployment
 if [ $INCLUDE_RUN_SH -gt 0 ]; then
-	cp $SCRIPT_PATH/../stages/push/post-deploy/run.sh "$DEPLOY_PACKAGE_DIR/$DS_DIR"
+	cp -v $SCRIPT_PATH/../stages/push/post-deploy/run.sh "$DEPLOY_PACKAGE_DIR/$DS_DIR"
 	printf "RESTART_COMMAND=\"sh ./$DS_DIR/run.sh restart\"\n" >> "$DEPLOY_CONFIG_SH"
 fi
 
