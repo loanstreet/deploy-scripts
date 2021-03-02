862ca1a8141ade0209553d43c825ad70cf89be8c
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2021-03-02 12:57:12 +0800

    when pushing deployment to git repo, use the project env as branch name unless specified otherwise

--- steps/push/lib/git.sh
+++ steps/push/lib/git.sh
@@ -4,6 +4,10 @@ ds_push() {
 	fi
 
 	cd "$1"
+	if [ "$DEPLOY_BRANCH" = "" ]; then
+		DEPLOY_BRANCH=$PROJECT_ENVIRONMENT
+	fi
+	git checkout -b $DEPLOY_BRANCH
 	info "Deploying $PROJECT_ENVIRONMENT to $DEPLOYMENT_SERVER"
 	git remote add deploy $DEPLOYMENT_SERVER 2>&1 | indent
 	GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push -u deploy $DEPLOY_BRANCH -f
