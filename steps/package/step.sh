558680ca319771b4278b6704c2e649d314c814ac
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2021-04-24 21:11:55 +0800

    delete other env dirs when deploying env

--- steps/package/step.sh
+++ steps/package/step.sh
@@ -1,4 +1,14 @@
 ds_exec_step() {
+	ENVS_DIR="$DEPLOY_PACKAGE_DIR/$DS_DIR/environments/"
+	if [ -d "$ENVS_DIR" ]; then
+		ENVS_LIST=$(ls "$ENVS_DIR" | cut -d";" -f1)
+		for m in $ENVS_LIST; do
+			if [ "$m" != "$PROJECT_ENVIRONMENT" ]; then
+				rm -rf "$ENVS_DIR/$m"
+			fi
+		done
+	fi
+
 	if [ "$PACKAGE" != "" ]; then
 		# Package the deployment files in the desired format using ds_package() to be ready for delivery to deployment target
 		title "package: $PACKAGE"
@@ -15,7 +25,6 @@ ds_exec_step() {
 		title 'build - post build script'
 		sh "$DEPLOY_PACKAGE_DIR/deploy/post_build.sh"
 	fi
-
 
 	cd $BUILD_REPO
 	# Quit if no target server is specified for delivering the deployment to
