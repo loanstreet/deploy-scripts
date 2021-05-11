5f531ba6c5b04ca125c4145ac1b5b77e105d5cd8
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2021-05-11 18:01:27 +0800

    enable push to multiple remote repos

--- steps/push/lib/git.sh
+++ steps/push/lib/git.sh
@@ -8,7 +8,11 @@ ds_push() {
 		DEPLOY_BRANCH=$PROJECT_ENVIRONMENT
 	fi
 	git checkout -b $DEPLOY_BRANCH
-	info "Deploying $PROJECT_ENVIRONMENT to $DEPLOYMENT_SERVER"
-	git remote add deploy $DEPLOYMENT_SERVER 2>&1 | indent
-	GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push -u deploy $DEPLOY_BRANCH -f
+	SERVER_LIST=$(echo $DEPLOYMENT_SERVER | cut -d";" -f1)
+	for b in $SERVER_LIST; do
+		info "Deploying $PROJECT_ENVIRONMENT to $b"
+		git remote add deploy $b 2>&1 | indent
+		GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push -u deploy $DEPLOY_BRANCH -f
+		git remote remove deploy 2>&1 | indent
+	fi
 }
