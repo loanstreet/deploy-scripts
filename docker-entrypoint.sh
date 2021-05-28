67505ccf46bff72ab30498cd61b2b11a5cd511f1
Ritesh Chitlangi <ritesh.chitlangi@loanstreet.com.my>
2021-05-29 02:57:47 +0800

    fixed docker usage command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ DEPLOY_SCRIPTS_HOME="$HOME/.deploy-scripts"
 VERSION=$(cat $DEPLOY_SCRIPTS_HOME/.VERSION)
 
 show_usage() {
-	printf "Usage:\n\tdocker run -it deploy-scripts:$VERSION -v [project directory path]:/project [environment name]\n"
+	printf "Usage:\n\tdocker run -v [project directory path]:/project -it deploy-scripts:$VERSION [environment name]\n"
 	exit 1
 }
 
